97bbc060985627e8950b8ba666dd9b1225a369b7
Data and Knowledge Engineering Lab, Kyung-Hee University <88026361+dke-lab@users.noreply.github.com>
2025-06-27 20:10:51 +0900

    Commit

---
+++ Cluster Configurations/ZOOKEEPER_CLIENT/zookeeper-env.sh
@@ -0,0 +1,9 @@
+
+export JAVA_HOME=/usr/jdk64/jdk1.8.0_112
+export ZOOKEEPER_HOME=/usr/hdp/current/zookeeper-client
+export ZOO_LOG_DIR=/var/log/zookeeper
+export ZOOPIDFILE=/var/run/zookeeper/zookeeper_server.pid
+export SERVER_JVMFLAGS=-Xmx1024m
+export JAVA=$JAVA_HOME/bin/java
+export CLASSPATH=$CLASSPATH:/usr/share/zookeeper/*
+
